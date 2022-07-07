4057274a8dad39132e405ee7ba3e48ff793936ce
alfydape <daffareri6@gmail.com>
2022-07-07 14:31:08 +0800

    tes lagi jes

--- resources/views/user/dashboard/udashboard/index.blade.php
+++ resources/views/user/dashboard/udashboard/index.blade.php
@@ -135,7 +135,7 @@ $respon_users = $all_respon_users->where('kartu_keluarga_id', Auth::user()->kart
     $belum_sehat = 0;
     foreach($all_respon_users as $all_respon_user){ //ini logic buat ngitung data dari masing" user yang nantinya dimasukin ke variabel $sehat sama $belum_sehat
     // $perbandingan = (($kuisoner->count() * 3) / 2) - ($all_respon_user->skor_nol * 3);
-    $rata_rata_skor = ($all_respon_user->total_skor)/($kuisoner - $all_respon_user->skor_nol);
+    $rata_rata_skor = ($all_respon_user->total_skor)/($kuisoner->where('ppemantauan_id', $all_respon_user->ppemantauan_id)->count() - $all_respon_user->skor_nol);
     $perbandingan = '2';
     $total_skor_user = 0;
     foreach($all_respon_users as $keluarga_respon){
