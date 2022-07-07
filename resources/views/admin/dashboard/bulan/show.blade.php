212f2b1d1d2b242a3cb488a0ca512722b11810e6
alfydape <daffareri6@gmail.com>
2022-07-07 14:27:35 +0800

    done bulan grafik

--- resources/views/admin/dashboard/bulan/show.blade.php
+++ resources/views/admin/dashboard/bulan/show.blade.php
@@ -12,14 +12,16 @@
 $sehat = 0;
 $belum_sehat = 0;
 foreach($all_respon_users as $all_respon_user){ //ini logic buat ngitung data dari masing" user yang nantinya dimasukin ke variabel $sehat sama $belum_sehat
-$perbandingan = (($kuisoner->count() * 3) / 2) - ($all_respon_user->skor_nol * 3);
+$rata_rata_skor = ($all_respon_user->total_skor)/($kuisoner->where('ppemantauan_id', $all_respon_user->ppemantauan_id)->count() - $all_respon_user->skor_nol);
+$perbandingan = '2';
+// $perbandingan = (($kuisoner->count() * 3) / 2) - ($all_respon_user->skor_nol * 3);
 $total_skor_user = 0;
 foreach($all_respon_users as $keluarga_respon){
 if($keluarga_respon->kartu_keluarga_id == $all_respon_user->kartu_keluarga_id){
 $total_skor_user = $total_skor_user + $keluarga_respon->total_skor;
 }
 }
-if($total_skor_user > $perbandingan){
+if($rata_rata_skor >= $perbandingan){
 $sehat++;
 } else {
 $belum_sehat++;
